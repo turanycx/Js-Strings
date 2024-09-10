67bef5722d2506a17dbe22dbe5c2b0daa602c1d5
turanycx <turanycc@gmail.com>
2024-09-10 14:20:18 +0300

    Add files via upload

---
+++ StringMethods/main.js
@@ -0,0 +1,11 @@
+console.log("Merhaba Dünya");
+
+let ad="Turan";
+let soyad="Yılancıoğlu";
+let adSoyad=ad+" "+soyad;
+console.log(adSoyad);
+
+console.log(adSoyad.length);
+
+let index=adSoyad.indexOf("ğ");
+console.log("ğ harfinin bulunduğu index: "+ index);
